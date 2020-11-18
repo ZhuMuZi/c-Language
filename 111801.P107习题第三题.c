a69eb29b295f27dc4dc406dbde92d4148eddff18
ZhuMuZi <74294869+ZhuMuZi@users.noreply.github.com>
2020-11-18 09:00:42 +0800

    Create 111801.P107习题第三题.c

---
+++ 111801.P107习题第三题.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int a=3,b=4,c=5;
+ int x,y;
+ printf("%d\n",a+b>c&&b==c);
+ printf("%d\n",a||b+c&&b-c);
+ printf("%d\n",a||b+c&&b-c);
+ printf("%d\n",a||b+c&&b-c);
+ printf("%d\n",a||b+c&&b-c);
+ return 0;
+}
