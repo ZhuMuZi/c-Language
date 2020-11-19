54fdb9f15041424e4f7ad4d1444559bae2317448
ZhuMuZi <74294869+ZhuMuZi@users.noreply.github.com>
2020-11-19 19:47:31 +0800

    Update 111910.性别.c

--- 111910.性别.c
+++ 111910.性别.c
@@ -4,11 +4,14 @@ int main()
 	int a;
 	printf("输入1或0\n");
 	scanf("%d",&a);
-	if(a){
+	if(a==1){
 		printf("男\n");
 	}
-	else{
+	else if(a==0){
 		printf("女\n");
+	}
+	else {
+		printf("输入有误，请重新输入\n");
 	}
 	return 0;
 }
