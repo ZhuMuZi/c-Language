b28a114a8949054570b37b3d5696680e37e4fd49
ZhuMuZi <74294869+ZhuMuZi@users.noreply.github.com>
2020-11-26 20:03:08 +0800

    Create 112603.升序排序.app.c

---
+++ 112603.升序排序.app.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+	printf("请输入三个数字(如：1 2 3)\n");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a<b&&a<c){
+		if(b<c){
+		printf("%d\n%d\n%d\n",a,b,c);
+		}
+		else{
+		printf("%d\n%d\n%d\n",a,c,b);
+		}
+	}
+	if(b<a&&b<c){
+		if(a<c){
+		printf("%d\n%d\n%d\n",b,a,c);
+		}
+		else{
+		printf("%d\n%d\n%d\n",b,c,a);
+		}
+	}
+	if(c<a&&c<b){
+		if(a<b){
+		printf("%d\n%d\n%d\n",c,a,b);
+		}
+		else{
+		printf("%d\n%d\n%d\n",c,b,a);
+		}
+	}
+	return 0;
+}
