2bc18012af2e19b555b4d17d5442c99cd887c8a1
ZhuMuZi <74294869+ZhuMuZi@users.noreply.github.com>
2020-12-02 17:20:43 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -1,37 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int seconds,minutes,hours;
+	int s,min,h;
 	printf("请输入秒数\n");
-	scanf("%d",&seconds);
-	minutes=seconds/60;
-	hours=seconds/3600;
-	seconds=seconds%60;
-	if(seconds<10){
-		if(minutes<10&&hours<10){
-			printf("0%d:0%d:0%d\n",hours,minutes,seconds);
-		}
-		else if(minutes>=10){
-			if(hours<10){
-				printf("0%d:%d:0%d\n",hours,minutes,seconds);
-			}
-			else{
-				printf("%d:%d:0%d\n",hours,minutes,seconds);
-			}
-		}
-	}
-	else{
-		if(minutes<10&&hours<10){
-			printf("0%d:0%d:%d\n",hours,minutes,seconds);
-		}
-		else if(minutes>=10){
-			if(hours<10){
-				printf("0%d:%d:%d\n",hours,minutes,seconds);
-			}
-			else{
-				printf("%d:%d:%d\n",hours,minutes,seconds);
-			}
-		}
-	}
+	scanf("%d",&s);
+	min=s/60;
+	h=s/3600;
+	s=s%60;
+	printf("%.2d:%.2d:%.2d\n",h,min,s);
 	return 0;
 }
