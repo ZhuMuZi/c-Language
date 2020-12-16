d848f7e1da2645de609d007c259c51912a3cb859
ZhuMuZi <74294869+ZhuMuZi@users.noreply.github.com>
2020-12-16 09:11:32 +0800

    Update 121602.常规求和.app.c

--- 121602.常规求和.app.c
+++ 121602.常规求和.app.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int x,i,a;
+	int x;
 	int y=0;
 	printf("请输入一个大于0的数\n");
 	while(1){
@@ -10,9 +10,9 @@ int main()
 		printf("错误，请输入一个大于0的数\n");
 	}
 	else {
-		for(i=0;i<=x;i++){
-		  y=y+i;
-	  }
+		for(x;x>=0;x--){
+		  y=y+x;
+		}
 	}
 	printf("%d\n",y);
 	y=0;
